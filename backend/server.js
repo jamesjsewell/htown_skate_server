62426579421aa442bec0e37b0f2ba3841c2dd987
james sewell <jamesjosephsewell@gmail.com>
2018-05-19 23:16:45 -0500

    fixed cors

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // import sub routers for CRUD api
 let router = require("./api_features/router.js")
 
+app.all('*', function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+	res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    next()
+});
+
 // base router for CRUD api
 app.use("/", router)
 
