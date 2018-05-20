7257bbaa041315ea996b036d12894607a39fcb72
james sewell <jamesjosephsewell@gmail.com>
2018-05-20 12:33:40 -0500

    added success status to success response object

--- backend/api_features/items/on_success.js
+++ backend/api_features/items/on_success.js
@@ -2,6 +2,6 @@ module.exports = {
     // takes the request from the http request, the response object, message to client, data output
     // returns it back to the client
     success: function(req, res, msg, output){
-        res.json(output)
+        res.status(201).json(output)
     }
 }
